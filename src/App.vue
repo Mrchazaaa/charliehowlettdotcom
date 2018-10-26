94c1f31826f62874bffaefcd3728a5768fa39266
mrchazaaa <charlie.howlett@live.co.uk>
2018-10-26 23:48:06 +0100

    added p5 sketch as background

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="big website coming soon"/>
-    <CharliesWorld />
     <div class='sketch' id='sketch'></div>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="big website coming soon"/>
+      <CharliesWorld />
   </div>
 </template>
 
 
 <script>
-import sketch from './javascript/background.js'
-import p5 from 'p5'
-
 import HelloWorld from './components/HelloWorld.vue'
 import CharliesWorld from './components/CharliesWorld.vue'
+import sketch from './javascript/background.js'
+import p5 from 'p5'
 
 export default {
   name: 'app',
@@ -23,7 +22,7 @@ export default {
   },
   data() {
     return {
-      myp5: null,
+      myp5: null
     }
   },
   mounted() {
@@ -33,13 +32,18 @@ export default {
 </script>
 
 <style>
-
+body, html {
+    margin:0; 
+    padding:0;
+}
 #sketch {
-   width: 100%;
-   height: 100%;
-   position: absolute;
-   left: 0;
-   top: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  margin: 0; 
+  z-index: -1;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -47,6 +51,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
--- src/javascript/background.js
+++ src/javascript/background.js
@@ -2,12 +2,15 @@ export default function( sketch ) {
     var width = 0;
     var height = 0;
     sketch.setup = function() {
-    width = document.getElementById('sketch').clientWidth;
-    height = document.getElementById('sketch').clientHeight;
-    sketch.createCanvas(width, height);
+        var cnv = sketch.createCanvas(document.body.clientWidth, document.body.scrollHeight);//sketch.windowHeight);
+        // cnv.parent("sketch");
     }
     sketch.draw = function() {
         sketch.fill(0);
         sketch.ellipse(50, 50, 80, 80);
+        sketch.background(255, 204, 0);
+    }
+    sketch.windowResized = function() {
+        sketch.resizeCanvas(document.body.clientWidth, document.body.scrollHeight);
     }
 }
