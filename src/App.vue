c1a3268e87d2f74db72ff21ec814a6077db90282
mrchazaaa <charlie.howlett@live.co.uk>
2018-11-03 16:18:52 +0000

    backup before moving all content inside bootstrap grid

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,11 @@
 
     <div class="container">
         <div class="row"> 
-            <div class="col-12  col-md-3">
-              1 of 3
-            </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12  col-md-1"></div>
+            <div class="col-12 col-md-10">
               2 of 3
             </div>
-            <div class="col-12 col-md-3">
-              3 of 3
-            </div>
+            <div class="col-12 col-md-1" style="background-color: transparent;"></div>
         </div>
     </div>
 
@@ -63,6 +59,20 @@
     text-align: center;
     color: #2c3e50;
 }
+body, html {
+    margin:0; 
+    padding:0;
+}
+.container{
+    width: 100%;
+    margin-left: 0;
+    margin-right: 0;
+    margin:0;
+}
+.row > div{
+    background-color: transparent;
+}
+.row > div:nth-child(2){
+    background-color: #f0ead6;
+}
 </style>
-
-<style src="./externalcss/main.css"></style>
